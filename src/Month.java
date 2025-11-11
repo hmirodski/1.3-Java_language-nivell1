57fe6b0134422196857fe5e2fe085c6393059a58
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-11-11 12:55:49 +0100

    First Commit
    Project Creation
    
    Feat: Creation class Month

---
+++ src/Month.java
@@ -0,0 +1,13 @@
+
+public class Month {
+
+    private String name;
+
+    public Month(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
