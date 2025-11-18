a9e8a1955049d11ebe35ee1a202084adeea0daa0
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-11-18 11:55:18 +0100

    feat: created hashmap

--- src/exercici3/Main.java
+++ src/exercici3/Main.java
@@ -1,9 +1,12 @@
 package exercici3;
 
 import java.io.File;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
+
+        HashMap<String,String> countries = new HashMap<>();
 
 
     }
