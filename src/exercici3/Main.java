c516e4a990249c8fd114e838428402c24e5642f1
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-11-18 12:06:15 +0100

    feat: created buffered reader

--- src/exercici3/Main.java
+++ src/exercici3/Main.java
@@ -1,12 +1,22 @@
 package exercici3;
 
-import java.io.File;
+import java.io.*;
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
 
         HashMap<String,String> countries = new HashMap<>();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("countries.txt"));
+            String line;
+            line = reader.readLine();
+            
+
+        }  catch (IOException e) {
+            System.out.println(e);
+        }
 
 
     }
