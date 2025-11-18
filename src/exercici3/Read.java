f907e81e6e982d175e3fd76e4f3aa69f6009f624
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-11-18 11:23:07 +0100

    feat: created filereader with exception

--- src/exercici3/Read.java
+++ src/exercici3/Read.java
@@ -1,12 +1,17 @@
 package exercici3;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class Read {
 
     public void read(){
-        
-        FileReader countries = new FileReader("countries.txt");
+
+        try {
+            FileReader countries = new FileReader("acountries.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("Archivo no encontrado");
+        }
     }
 
 }
