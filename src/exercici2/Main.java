1049b39773164b28d4e921582476a24dba2d492f
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-11-18 10:46:32 +0100

    feat: created main and first list

---
+++ src/exercici2/Main.java
@@ -0,0 +1,22 @@
+package exercici2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> lista = new ArrayList<>();
+        lista.add(1);
+        lista.add(2);
+        lista.add(3);
+        lista.add(4);
+        lista.add(5);
+        System.out.println(lista);
+
+        List<Integer> segonaLista = new ArrayList<>();
+        
+    }
+}
