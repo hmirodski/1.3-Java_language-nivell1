93343bac17b3c22081f6fe252ca74b46aad4a1ae
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-11-19 20:37:26 +0100

    fix deleting empty spaces

--- src/exercici3/Main.java
+++ src/exercici3/Main.java
@@ -6,23 +6,19 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        
         String filePath = "countries.txt";
-
 
         CountriesList countries = new CountriesList();
         Map<String,String> countryList = countries.loadCountries(filePath);
-
-
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter username: ");
         String username = scanner.nextLine();
 
-
         CountryQuiz game = new CountryQuiz(countryList);
         game.playRound(scanner);
         System.out.println(username + " You scored " + game.getPoints() + "/10 points");
-
 
         SaveFile saver = new SaveFile();
         if (saver.save("clasificacio.txt", username, game.getPoints())) {
@@ -30,7 +26,6 @@ public class Main {
         } else {
             System.out.println("classificacio.txt not saved");
         }
-
 
     }
 }
